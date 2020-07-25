113ad52f9b7add3f85bbea96f9d3d892a38f693d
donovanolivarez <34660331+donovanolivarez@users.noreply.github.com>
2020-07-25 17:28:25 -0500

    Updates

---
+++ precache-manifest.a1d9f47212c15fd8769b2a47a9bd1994.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "364e5941f774fe536213a5ceca67af24",
+    "url": "/pomosession/index.html"
+  },
+  {
+    "revision": "2cf05d8f3978941e6529",
+    "url": "/pomosession/static/css/main.c7cb8223.chunk.css"
+  },
+  {
+    "revision": "090b1482cc0ca0dda720",
+    "url": "/pomosession/static/js/2.73ab5bd6.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/pomosession/static/js/2.73ab5bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf05d8f3978941e6529",
+    "url": "/pomosession/static/js/main.3d70c4fb.chunk.js"
+  },
+  {
+    "revision": "60f7bd03574217d0710f",
+    "url": "/pomosession/static/js/runtime-main.08198af2.js"
+  },
+  {
+    "revision": "a514170e52d804424fc6912e19fbe1a6",
+    "url": "/pomosession/static/media/Bugle_Tune.a514170e.mp3"
+  }
+]);
